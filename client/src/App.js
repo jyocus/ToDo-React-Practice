3932aee6d05b2b517220f100a06caea5a9298cf8
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-21 11:50:53 -0600

    added todos state and props

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,30 @@ import './App.css';
 import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'Take out the Trash',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id:3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
   render() {
+    // console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
