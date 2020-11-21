6ae377274526435fa72eed5aa78862f3279b8d33
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-21 13:09:45 -0600

    changing state for completed t/f

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id:1,
+        id: 1,
         title: 'Take out the Trash',
         completed: false
       },
       {
-        id:2,
+        id: 2,
         title: 'Dinner with wife',
         completed: true
       },
       {
-        id:3,
+        id: 3,
         title: 'Meeting with boss',
         completed: false
       }
@@ -24,7 +24,16 @@ class App extends Component {
   }
 
   markComplete = (id) => {
-    console.log(id);
+    // console.log(id);
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+
+        }
+        return todo;
+      })
+    })
 
   }
 
